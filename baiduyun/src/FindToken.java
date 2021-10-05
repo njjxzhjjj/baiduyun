a5907bd292018c4dc7f8d016965f8e5b32c1ccd4
njjx <njjxzh10@163.com>
2021-10-05 19:14:27 +0800

    baiduyun

---
+++ baiduyun/src/FindToken.java
@@ -0,0 +1,9 @@
+public class FindToken {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String token=AuthService.getAuth();
+		System.out.println("token=" + token);
+	}
+
+}
